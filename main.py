3d2e42cf4b043f90ed0a9d12160cb300a79e86e9
StevenM <stevenbmendoza@gmail.com>
2023-03-25 16:05:17 -0700

    beautifulsoup module to parse <img>  website html data

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 
 # specify the URL of the website
-url = input("> Enter a website to extract the images from: ")
+url = input("> Enter a website's exact url page to extract the images from: ")
 
 # try to make a GET request to the specified URL, passing the 's' query parameter 
 # to search for 'requests+language:python'. if an exception occurs during the 
@@ -15,4 +15,7 @@ try:
 except requests.exceptions.RequestException as error: # catch any RequestException
     raise SystemExit(error)  # exit the program with the error message
 
-  
+# parse the HTML content of the website
+soup = BeautifulSoup(response.content, 'html.parser')
+# find all image tags on the website
+img_tags = soup.find_all('img')
